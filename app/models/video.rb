a2987235aa013e6141f5d7f2eaaa860db61a4e60
murakami <35243965+KanaeMurakami@users.noreply.github.com>
2021-06-21 22:42:20 +0900

    やってみよう！追加
    - Videoモデルでスコープとバリデーションを作ってみよう

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 class Video < ApplicationRecord
+  PAYMENT = %w[無料 見放題 有料].freeze
+
   validates :title, presence: true
+  validate :validation_payment
+
+  # genre: "映画" のビデオを絞り込むスコープを追加
+  scope :movies, lambda {
+  }
+
+  private
+
+  # payment には「無料/見放題/有料」のいずれかが入っていることを確かめるバリデーションを追加
+  def validation_payment
+    # errors.add(:property, :inclusion)
+  end
 end
