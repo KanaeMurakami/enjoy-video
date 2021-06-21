3b11502a512554bf9694e8b923b431c9433bc401
murakami <35243965+KanaeMurakami@users.noreply.github.com>
2021-06-21 23:40:31 +0900

    やってみよう！その３追加
    - Videoモデルでスコープとバリデーションを作ってみよう

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,4 +5,7 @@ class Payment < ApplicationRecord
 
   has_one :video_payment, dependent: :destroy
   has_one :video, through: :video_payment
+
+  # name には「無料/見放題/有料」のいずれかが入っていることを確かめるバリデーションを追加
+  # validates :name, ...
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -7,19 +7,8 @@ class Video < ApplicationRecord
   has_many :genres, through: :video_genres
 
   validates :title, presence: true
-  # payment には「無料/見放題/有料」のいずれかが入っていることを確かめるバリデーションを追加
-  # validates :payment, ...
 
-  validate :validation_genre_uniqueness
-
-  # genre: "映画" のビデオを絞り込むスコープを追加
+  # genre の name: "映画" のビデオを絞り込むスコープを追加
   scope :movies, lambda {
   }
-
-  private
-
-  # genre1 と genre2 に同じ名称が入らないようにバリデーションを実装したい
-  def validation_genre_uniqueness
-    # errors.add(:genre1, :taken) or errors.add(:genre2, :taken)
-  end
 end
