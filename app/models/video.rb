aef9c93fe9597ecfd043936caf15cdc3f7ee2a7f
murakami <35243965+KanaeMurakami@users.noreply.github.com>
2021-06-22 23:26:59 +0900

    やってみよう！その２回答例

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,18 +5,21 @@ class Video < ApplicationRecord
 
   validates :title, presence: true
   # payment には「無料/見放題/有料」のいずれかが入っていることを確かめるバリデーションを追加
-  # validates :payment, ...
+  validates :payment, inclusion: { in: PAYMENT }
 
   validate :validation_genre_uniqueness
 
   # genre: "映画" のビデオを絞り込むスコープを追加
   scope :movies, lambda {
+    where(genre1: '映画').or(where(genre2: '映画'))
   }
 
   private
 
   # genre1 と genre2 に同じ名称が入らないようにバリデーションを実装したい
   def validation_genre_uniqueness
-    # errors.add(:genre1, :taken) or errors.add(:genre2, :taken)
+    return if genre1 != genre2
+
+    errors.add(:genre2, :taken)
   end
 end
